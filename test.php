21d6d899db0e67cec13077a1bde2f56ef717de1e
Arthakker <arthuronyeanusi@gmail.com>
2025-12-09 18:21:49 +0100

    Improved UI for homepage

---
+++ test.php
@@ -0,0 +1,21 @@
+<?php 
+//echo "Hello World";
+
+//print_r([1,2,3]);
+
+// var_export('Hello')
+
+// $age = 20;
+// $name = 'Arthur';
+// $is_masturbates = true;
+
+// echo "${name}  is  ${age} years old" ;
+
+// define('HOST', 'localhost');
+
+// $HOST = "Aiven";
+// $HOST = 'MySQL';
+
+// echo $HOST;
+
+?>
